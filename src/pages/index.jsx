bad35a2c447b566e0e4e1bee4fcd8ade7701c715
Tiago Persch <thiagopersch@gmail.com>
2023-05-19 21:50:59 -0400

    Added modifications

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,37 @@
-import styled from "styled-components";
+import P from 'prop-types';
+import Head from 'next/head';
+import styled from 'styled-components';
+import config from '../config';
+import { mapData } from '../api/map-data';
+import Home from '../templates/Home';
+import { loadPages } from '../api/load-pages';
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.fontColor.primaryColor};
-`;
-
-export default function Home() {
-  return <Title>My page</Title>
+export default function Index({ data = null }) {
+  return <Home data={data} />;
 }
+
+export const getStaticProps = async () => {
+  let data = null;
+
+  try {
+    data = await loadPages('landing-page');
+  } catch (e) {
+    //
+  }
+
+  if (!data || !data.length) {
+    return {
+      notFound: true
+    };
+  }
+
+  return {
+    props: {
+      data
+    }
+  };
+};
+
+Index.propTypes = {
+  data: P.array
+};
